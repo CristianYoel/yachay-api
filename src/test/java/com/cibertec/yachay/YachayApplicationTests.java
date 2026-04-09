90fdff44d4e6be229fbbded87a928e32e274fb38
Cristian <cyoel467@gmail.com>
2026-04-08 22:10:06 -0500

    Primer commit - API

---
+++ src/test/java/com/cibertec/yachay/YachayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.yachay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YachayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
